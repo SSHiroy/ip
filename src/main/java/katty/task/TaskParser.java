5bf0e54030f284bff890e327231b2adb4dc44400
SSHiroy <shiroysattur.m@gmail.com>
2026-02-13 12:56:18 +0800

    docs: TaskParser

--- src/main/java/katty/task/TaskParser.java
+++ src/main/java/katty/task/TaskParser.java
@@ -3,8 +3,24 @@ package katty.task;
 import java.time.format.DateTimeParseException;
 
 import katty.KattyException;
+import katty.TaskManager;
 
+/**
+ * Parses user commands and converts them into {@link Task} objects.
+ * <p>
+ * Acts as a bridge between {@link katty.Katty} and {@link TaskManager},
+ * translating raw user input into concrete task instances.
+ */
 public class TaskParser {
+
+    /**
+     * Parses user commands and inputs.
+     *
+     * @param command decides how input is processed
+     * @param input user input
+     * @return an object of {@code Task}
+     * @throws KattyException if there is an invalid command or input
+     */
     public static Task parser(String command, String input) throws KattyException {
         switch (command) {
         case "todo" -> {
