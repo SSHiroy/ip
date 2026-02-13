c388a5fc8068d71dd580d5924221c9cda1bd4b12
SSHiroy <shiroysattur.m@gmail.com>
2026-02-13 12:57:14 +0800

    docs: ToDo

--- src/main/java/katty/task/ToDo.java
+++ src/main/java/katty/task/ToDo.java
@@ -1,5 +1,11 @@
 package katty.task;
 
+/**
+ * Represents a simple to-do {@link Task}.
+ * <p>
+ * A {@code ToDo} consists only of a task name and its completion status,
+ * without any associated date or time.
+ */
 public class ToDo extends Task {
     public ToDo(String taskName) {
         super(taskName);
