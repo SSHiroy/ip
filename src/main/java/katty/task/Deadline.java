1c1a43ca4ae35af5e02989d773e1e2ad85c294a6
SSHiroy <shiroysattur.m@gmail.com>
2026-02-13 12:46:17 +0800

    docs: Deadline

--- src/main/java/katty/task/Deadline.java
+++ src/main/java/katty/task/Deadline.java
@@ -3,11 +3,21 @@ package katty.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Extends an {@code Task}, as a deadline {@code Deadline} comes with a name, completion status and a deadline.
+ */
 public class Deadline extends Task {
     public static final String DEADLINE_FORMAT = "dd-MM-yyyy HH:mm";
     private static final DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern(DEADLINE_FORMAT);
     private LocalDateTime deadlineBy;
 
+    /**
+     * Creates a new {@code Deadline} with a name, deadline and completion status. <br>
+     * Deadlines are incomplete by default.
+     *
+     * @param deadlineName name of deadline
+     * @param deadlineBy datetime the deadline is due
+     */
     public Deadline(String deadlineName, String deadlineBy) {
         super(deadlineName);
         this.deadlineBy = LocalDateTime.parse(deadlineBy, dateTimeFormat);
